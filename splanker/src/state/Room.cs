62be8ffa3677fb765e5ecce81eb50ba7e0820de6
Seba <dese@student.chalmers.se>
2018-09-27 20:33:10 +0200

    Moved all the files and refactored room

---
+++ splanker/src/state/Room.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splanker
+{
+    class Room
+    {
+        public Entities Entities { get; } = new Entities();
+
+        public Room()
+        {
+        }
+
+    }
+
+    /// <summary>
+    /// Class which acts as a container class for the set of
+    /// Entities in a room.
+    /// </summary>
+    class Entities : IEnumerable<Entity>
+    {
+        private List<Entity> EntityList;
+
+        /// <summary>
+        /// Adds an entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <todo> Callbacks </todo>
+        public void Add(Entity entity)
+        {
+            EntityList.Add(entity);
+        }
+
+        public IEnumerator<Entity> GetEnumerator()
+        {
+            return EntityList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return EntityList.GetEnumerator();
+        }
+    }
+}
